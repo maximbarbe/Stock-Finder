475015c319f24099b5a562356184158cc9d4ab84
Maxim Barbe <barbe.maxim@gmail.com>
2023-07-22 19:12:06 -0400

    optimisation

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
-
-from web_funcs import get_source_code, get_stock_charts
+from selenium.webdriver.common.by import By
+from web_funcs import get_stock_charts, get_pages
 
 # Chrome driver settings
 chrome_options = Options()
 chrome_options.add_argument("--headless=new")
 chrome_options.page_load_strategy = "eager"
 
+driver = Chrome(options=chrome_options)
+driver.set_page_load_timeout(5)
+
 
 all_charts = {}
-initial_num = 1
-while True:
-    source_code = get_source_code(options=chrome_options, chart_num= initial_num, url=f"https://finviz.com/screener.ashx?v=211&f=sh_avgvol_o500,sh_relvol_o0.5,ta_pattern_channelup2&ft=4&r={initial_num}")
-    charts = get_stock_charts(source_code=source_code)
-    if len(charts) == 1 and list(charts.keys())[0] in all_charts.keys():
-        break
-    else:
-        # Add the stocks to all_charts.
-        all_charts = all_charts | charts
-        # Add 12 because there are 12 charts on a page. 
-        initial_num += 12
+
+
+source_codes = get_pages(driver, url=f"https://finviz.com/screener.ashx?v=211&f=sh_avgvol_o500,sh_relvol_o0.5,ta_pattern_channelup2&ft=4")
+charts = get_stock_charts(source_code=source_codes)
+print(charts)
+        
 
--- web_funcs.py
+++ web_funcs.py
@@ -1,37 +1,60 @@
 from bs4 import BeautifulSoup
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import time
 import requests
 
 
 # Scrape the source code
-def get_source_code(options: Options, chart_num: int, url: str) -> BeautifulSoup:
-    driver = Chrome(options=options)
-    driver.set_page_load_timeout(5)
+def get_pages(driver: Chrome,  url: str) -> list[BeautifulSoup]:
+    current_page = 1
+    html_source_codes = []
     try:
         # Visit the page
         driver.get(url=url)
     except:
         print("Error loading the page!")
         quit()
+    while True:
+        html_source_codes.append(extract_html(driver=driver))
+        next_page = check_for_next_page(driver, current_page)
+        
+        if next_page:
+            current_page += 1
+            next_page.click()
+        else:
+            break
+    return html_source_codes
+
+def check_for_next_page(driver: Chrome, current_page: int):
+    pages = driver.find_elements(By.CLASS_NAME, "screener-pages")
+    for page in pages:
+        if page.text == str(current_page + 1):
+            return page
+    return None
+
+
+
+def extract_html(driver: Chrome) -> BeautifulSoup:
     # Execute a javascript script to get the source code
     source_code = driver.execute_script("return document.body.innerHTML")
     # Parse the code.
     html_code: BeautifulSoup = BeautifulSoup(source_code, "html.parser")
-    driver.quit()
     return html_code
 
 # Create a dict for the stock charts and tickers.
 def get_stock_charts(source_code: BeautifulSoup) -> dict:
     charts_url = {}
     # Find all anchor tags with 'border-text', that's how we find the stocks.
-    stocks = source_code.find_all("a", {"class": "border-text"})
-    for stock in stocks:
-        # Extract the ticker and the image link from the anchor tag.
-        ticker = get_ticker(stock)
-        img_link = get_img_link(stock)
-        charts_url[ticker] = img_link
+    for source in source_code:
+        stocks = source.find_all("a", {"class": "border-text"})
+        for stock in stocks:
+            # Extract the ticker and the image link from the anchor tag.
+            ticker = get_ticker(stock)
+            img_link = get_img_link(stock)
+            if ticker not in charts_url:
+                charts_url[ticker] = img_link
+
     return charts_url
 
 # Function to get the ticker from the alternate name of the image
